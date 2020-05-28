27440fb0d2afef009d7127d75b2b0755a8ed1e54
root <root@instance-1.us-central1-a.c.learning-261705.internal>
2020-05-28 15:49:41 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var handleRequest = function(request, response) {
+	response.writeHead(200);
+	response.end("<h1> Deployed Version is v1.0 </h1>");
+}
+var www = http.createServer(handleRequest);
+www.listen(8080);
+
